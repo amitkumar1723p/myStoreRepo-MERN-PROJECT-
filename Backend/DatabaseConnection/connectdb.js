7876510571ce72181216e96a0031cddf10265a17
Amit <amitkumar1723p@gmail.com>
2023-04-07 16:59:32 +0530

    remove useless commet

--- Backend/DatabaseConnection/connectdb.js
+++ Backend/DatabaseConnection/connectdb.js
@@ -1,21 +1,9 @@
 import mongoose from "mongoose";
 
-//  Database Connection Code 
 const connectDb = async () => {
-    // const DB_OPTIONS = {
-    //     // user: process.env.USER,
-    //     // pass: process.env.PASSWORD,
-    //     dbName: process.env.DATABASE_NAME,
-    //     // authSource: process.env.AUTH_SOURCE
-    // }
-    const connectionUri = process.env.DATABASE_URI
+  const connectionUri = process.env.DATABASE_URI;
 
-    mongoose.set('strictQuery', true)
-    await mongoose.connect(connectionUri)
-
-
-
-
-
-}
-export default connectDb
+  mongoose.set("strictQuery", true);
+  await mongoose.connect(connectionUri);
+};
+export default connectDb;
